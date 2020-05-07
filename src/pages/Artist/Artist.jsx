fde54d803b46a529962ecc66651430e0d35a16fa
Frank Rauen <home@Franks-MacBook-Air.local>
2020-05-07 16:50:06 -0600

    adds xml api access

--- src/pages/Artist/Artist.jsx
+++ src/pages/Artist/Artist.jsx
@@ -1,16 +1,36 @@
 import React, { Component } from 'react';
+import XMLViewer from 'react-xml-viewer';
 import styles from './Artist.module.css';
 
 class Artist extends Component {
     state = {
         tabs: []
     }
+
+    handleGetTabs = async searchTerm => {
+        try {
+        const apiUrl = `https://www.songsterr.com/a/ra/songs/byartists.xml?artists=${searchTerm}`
+        const response = await fetch(apiUrl);
+        const { tabs } = response
+        this.setState({ tabs })
+        } catch (error) {
+            alert('Artist Not Found');
+        }
+    }
+
+    componentDidMount() {
+        this.handleGetTabs('metallica');
+    }
+
     render() {
         return(
             <div>
             <h1>Search Tab by Artist</h1>
-            <input type="text" />
-            <button>Search</button>
+            <form onSubmit = {this.handleGetTabs}>
+            <input type="text"
+            ref={(searchTerm) => this.searchTerm = searchTerm}/>
+            <XMLViewer  />
+            </form>
             </div>
         )
     }
